c64748d4e6bbe5f4c1c2e2bc5bd325a65610374a
Vindex <vindex.pg@gmail.com>
2021-04-26 02:34:15 +0100

    Cambios para pantalla pequeña y add practica .xml

--- 000webhost/public_html/js/javaScript.js
+++ 000webhost/public_html/js/javaScript.js
@@ -1,14 +1,45 @@
-window.sr =  ScrollReveal();
-      sr.reveal('.navbar', {
-        duration: 3000,
-        origin: 'top', 
-        distance: '100px'      
-      });
-      sr.reveal('.titulo', {
-        duration: 3000,
-        origin: 'right',
-        distance: '300px'       
-      });
-      sr.reveal('.enlace', {
-        duration: 3000,
-      });
+
+if (screen.width < 1024){
+  console.log('width < 1024px')
+  window.sr =  ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 3000,     
+  });
+  sr.reveal('.titulo', {
+    duration: 3000,       
+  });
+  sr.reveal('.enlace', {
+    duration: 3000,
+  });
+}else if (screen.width < 1280){
+  console.log('width < 1280px')
+  window.sr =  ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 3000,     
+  });
+  sr.reveal('.titulo', {
+    duration: 3000,       
+  });
+  sr.reveal('.enlace', {
+    duration: 3000,
+  });
+}else{ 
+  console.log('width > 1280px')
+  window.sr =  ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 3000,
+    origin: 'top', 
+    distance: '100px'      
+  });
+  sr.reveal('.titulo', {
+    duration: 3000,
+    origin: 'right',
+    distance: '300px'       
+  });
+  sr.reveal('.enlace', {
+    duration: 3000,
+  });
+}
+
+
+      
